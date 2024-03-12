f5ef8d5b5e4df4753b396af00d2b0613645d2c4e
Lingard <quanglinhptit1119@gmail.com>
2024-03-12 14:20:10 +0700

    bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "dev.note.notebook.notepad.wisenotes"
         minSdk = 24
         targetSdk = 33
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode =  6
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/example/colorphone/ui/edit/bottomBackgroundEdit/adapter/FragmentPagerAdapter.kt
+++ app/src/main/java/com/example/colorphone/ui/edit/bottomBackgroundEdit/adapter/FragmentPagerAdapter.kt
@@ -5,11 +5,11 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.colorphone.model.Background
 import com.example.colorphone.ui.edit.bottomBackgroundEdit.vp2.ListBackgroundFragment
 
-class FragmentPagerAdapter(fragment: Fragment, private var bgClick: (Background) -> Unit) : FragmentStateAdapter(fragment) {
+class FragmentPagerAdapter(fragment: Fragment, private var currentBg: Int, private var bgClick: (Background) -> Unit) : FragmentStateAdapter(fragment) {
 
     override fun getItemCount(): Int = 4
 
-    override fun createFragment(position: Int): Fragment = ListBackgroundFragment(position) {
+    override fun createFragment(position: Int): Fragment = ListBackgroundFragment(currentBg = currentBg, currentCate = position) {
         bgClick(it)
     }
 
